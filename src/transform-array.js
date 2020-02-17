8a4c77a9af12238b423e3cee20c3824ad1814e5f
Aliaksandr Rebkavets <aleksschpak@gmail.com>
2020-02-17 13:40:37 +0300

    feat: add transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,16 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if(arr.length === 0) return [];
+  if(Array.isArray(arr) != true) throw new Error();
+for(let i = 0; i < arr.length; i++) {
+  if(arr[i] === '--discard-next') {
+    arr.splice(arr[i - 1], 2);
+  } else if(arr[i] === '--discard-prev') {
+    arr.splice(arr[i -2], 2);
+} else if(arr[i] ===  '--double-next') {
+arr[i] = arr[i + 1];
+} else if(arr[i] === '--double-prev') {
+  arr[i] = arr[i - 1];
+}
+}
+  return arr;
 };
