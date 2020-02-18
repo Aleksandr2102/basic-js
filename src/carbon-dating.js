6ce51cb1380940b2fa621f19028fabd5e2436bc0
Aliaksandr Rebkavets <aleksschpak@gmail.com>
2020-02-18 15:12:07 +0300

    fix: correct solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,12 +1,10 @@
-const MODERN_ACTIVITY= 15; 
+ const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  if(sampleActivity === "string" && 0 < sampleActivity < MODERN_ACTIVITY){
-let t;
-  let k = 0.693 / HALF_LIFE_PERIOD;
-  return t = Math.ceil(Math.log((MODERN_ACTIVITY / parseFloat(sampleActivity))/ k));
-  } else {
-    return false;
-  }
-};
+  if(typeof sampleActivity !== "string") return false;
+  if(isNaN(parseFloat(sampleActivity))  === true ) return false;  
+  if(0 >= sampleActivity ||  sampleActivity > 15) return false;
+  let t = Math.ceil(Math.log(MODERN_ACTIVITY / parseFloat(sampleActivity)) /(0.693 / 5730));
+  return t;
+}
